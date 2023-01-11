44f6fd5e0688b0a9414663e093d6c50e1e16b328
Minh8x8 <105042689+Minh8x8@users.noreply.github.com>
2023-01-11 16:10:47 +0700

    my first commit

--- hello.js
+++ hello.js
@@ -5,8 +5,8 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.setHeader('Content-Type', 'text/html');
+    res.end("<h1>Hello World</h1><p>This is a HTMLresponse</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
 });
 
 server.listen(port, hostname, () => {
