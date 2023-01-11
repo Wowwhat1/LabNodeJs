335de5711e719ec06a682eec023934523ca34bdb
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-01-11 16:17:40 +0700

    Day2

--- hello.js
+++ hello.js
@@ -1,12 +1,23 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end("<h1>Hello World</h1><p>This is a HTMLresponse</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
+    switch (req.url) {
+        case "/home":
+            res.writeHead(200);
+            res.end("<h1>This is Home page</h1>");
+            break;
+        case "/about":
+            res.writeHead(200);
+            res.end("<h1>This is About page</h1>");
+            break;
+        default:
+            break;
+    }
 });
 
 server.listen(port, hostname, () => {
